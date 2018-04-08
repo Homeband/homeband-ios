3ad80a75e3c45f1b6ebad4f2ff96339cf43fa327
Nicolas Gérard <nicolas.gerard@proximus.be>
2018-04-08 16:41:03 +0200

    Création d'un fichiers pour étendre différentes classes swift natives

--- HomeBand/Tools/MyExtensions.swift
+++ HomeBand/Tools/MyExtensions.swift
@@ -25,3 +25,24 @@ extension UIColor{
     
     static let hbRed = UIColor(hex:0xCE2828)
 }
+
+extension UIApplication {
+    // Fonction qui renvoi la vue courante
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        
+        return base
+    }
+}
