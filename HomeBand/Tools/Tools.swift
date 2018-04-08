3b94808718490dd79b4b308167c5eff19f7bc8b1
Nicolas Gérard <nicolas.gerard@proximus.be>
2018-04-08 16:44:29 +0200

    Création de plusieurs fonctions "outils"
    - getHeaders()  : crée les headers nécessaire pour contacter l'API Homeband
    - checkStylesUpdate(...) : Vérifie si une mide à joru des styles est disponible dans l'API
    - Idem pour les villes

--- HomeBand/Tools/Tools.swift
+++ HomeBand/Tools/Tools.swift
@@ -9,11 +9,50 @@
 import Foundation
 import Realm
 import RealmSwift
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
+import CryptoSwift
 
 
 class Tools {
+    static let BASE_API_URL : String! = "http://dev.zen-project.be/homeband-api/api/"
     
-    static func getConnectedUser() -> Utilisateur?
+    class func getHeaders() -> HTTPHeaders{
+
+        let AK:String = "AK"
+        let AS:String = "AS"
+        let now:Int = Int(NSDate().timeIntervalSince1970)
+        var CK:String = ""
+        var headers:HTTPHeaders = HTTPHeaders()
+        let user:Utilisateur? = getConnectedUser()
+        var sign:String
+        
+        if(user != nil){
+            CK = (user?.api_ck)!
+        }
+        
+        // Application Key
+        headers.updateValue(AK, forKey: "X-Homeband-AK")
+        
+        // Timestamp
+        headers.updateValue(String(now), forKey: "X-Homeband-TS")
+        
+        // Customer Key
+        headers.updateValue((user?.api_ck)!, forKey: "X-Homeband-CK")
+        
+        // Signature
+        sign = "$1$" + (AS + "+" + String(now) + "+" + CK).sha256()
+        headers.updateValue(sign, forKey: "X-Homeband-SIGN")
+        
+        return headers
+    }
+    
+    class func isUserConnected() -> Bool {
+        return (getConnectedUser() != nil)
+    }
+    
+    class func getConnectedUser() -> Utilisateur?
     {
         // On vérifie si un utilisateur est connecté ou non
         let realm = try! Realm()
@@ -25,7 +64,7 @@ class Tools {
         }
     }
     
-    static func disconnectUser() -> Bool
+    class func disconnectUser() -> Bool
     {
         let connectedUser: Utilisateur? = self.getConnectedUser()!
         
@@ -39,4 +78,52 @@ class Tools {
         
         return true
     }
+    
+    class func checkStylesUpdate(update:Bool = false, displayAlert:Bool = false){
+        let urlCheck = Tools.BASE_API_URL + "versions"
+        let params:Parameters = [
+            "nomtable" : "STYLES"
+        ]
+        
+        // Récupération de la date de la dernière version de la table style
+        Alamofire.request(urlCheck, method: .get, parameters: params, headers: Tools.getHeaders()).responseJSON{response in
+            if(response.result.isSuccess){
+                let resultat = response.result.value as! [String:Any]
+                let versionAPI:Version = Mapper<Version>().map(JSONObject: resultat["version"])!
+                debugPrint(versionAPI)
+                
+                let realm = try? Realm()
+                let condition = NSPredicate(format: "nom_table like '%@'", versionAPI.nom_table)
+                let versionLocale:Version? = (realm?.objects(Version.self).filter(condition).first)
+                
+                
+                if(versionLocale == nil || versionAPI.date_maj > (versionLocale?.date_maj)!){
+                    if(update == true){
+                        
+                    } else if (displayAlert == true){
+                        let titre = "Mise à jour disponible"
+                        let message = "Une mise à jour des données est disponible. Pour mettre à jour les données, rendez-vous dans les paramètres de l'application."
+                        let alert:UIAlertController = UIAlertController(title: titre, message: message, preferredStyle: .alert)
+                        let actionOK:UIAlertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        
+                        alert.addAction(actionOK)
+                        
+                        let view = UIApplication.topViewController()
+                        if(view != nil){
+                            view?.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    class func checkVillesUpdate(update:Bool = true) -> Bool{
+        return true;
+    }
+    
+    class func checkGroupUpdate(id_groupes:Int = 0, update:Bool = false) -> Bool{
+        return true;
+    }
+    
 }
